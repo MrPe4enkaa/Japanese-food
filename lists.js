4dc2b955e09db4471b446e0a60ba9440d741dd6a
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2024-11-26 20:52:48 +0300

    Add files via upload

--- lists.js
+++ lists.js
@@ -5,15 +5,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let list = document.querySelector('.toggle');
     $(list).hide(0);
     $(button).on('click', () => {
+        if (gsap.isTweening(button)) return;
         $(list).slideToggle(400);
         count++;
         gsap.to(button, {
             rotation: '+=90',
-            duration: 0.4
+            duration: 0.4,
         });
         if (count % 2 === 0) {
             gsap.fromTo(list, {
-                y: -90,
+                y: -80,
                 opacity: 0
             }, {
                 y: 0,
@@ -23,9 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         } else {
             gsap.to(list, {
-                y: -30,
+                y: -40,
                 opacity: 0,
-                duration: 0.4,
+                duration: 0.5,
             })
         }
     });
